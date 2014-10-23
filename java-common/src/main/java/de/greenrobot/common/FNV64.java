e66cd67a0974e8d7209d9477ddff6414dc54f49f
Markus <markus@greenrobot>
2014-10-23 19:12:46 +0200

    added FNV64

---
+++ java-common/src/main/java/de/greenrobot/common/FNV64.java
@@ -0,0 +1,37 @@
+package de.greenrobot.common;
+
+import java.util.zip.Checksum;
+
+/** Hash function FNV-1a (http://www.isthe.com/chongo/tech/comp/fnv). */
+public class FNV64 implements Checksum {
+    private  final static long INITIAL_VALUE = 0xcbf29ce484222325L;
+    private  final static long MULTIPLIER = 0x100000001b3L;
+
+    private long hash = INITIAL_VALUE;
+
+    @Override
+    public void update(int b) {
+        hash ^= 0xffL & b;
+        hash *= MULTIPLIER;
+    }
+
+    @Override
+    public void update(byte[] b, int off, int len) {
+        int stop = off + len;
+        for (int i = off; i < stop; i++) {
+            hash ^= 0xffL & b[i];
+            hash *= MULTIPLIER;
+        }
+    }
+
+    @Override
+    /** Note: Java's long is signed. */
+    public long getValue() {
+        return hash;
+    }
+
+    @Override
+    public void reset() {
+        hash = INITIAL_VALUE;
+    }
+}
--- java-common/src/test/java/de/greenrobot/common/FnvTest.java
+++ java-common/src/test/java/de/greenrobot/common/FnvTest.java
@@ -4,19 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FnvTest {
-    private final static byte[] INPUT_ZERO1 = {(byte) 0xcc, 0x24, 0x31, (byte) 0xc4};
-    private final static byte[] INPUT_ZERO2 = {(byte) 0xe0, 0x4d, (byte) 0x9f, (byte) 0xcb};
+    private final static byte[] INPUT32_ZERO1 = {(byte) 0xcc, 0x24, 0x31, (byte) 0xc4};
+    private final static byte[] INPUT32_ZERO2 = {(byte) 0xe0, 0x4d, (byte) 0x9f, (byte) 0xcb};
+    private final static byte[] INPUT64_ZERO = {(byte) 0xd5, 0x6b, (byte) 0xb9, 0x53, 0x42, (byte) 0x87, 0x08, 0x36};
 
     @Test
     public void testFnv32UpdateZeroHash() {
         FNV32 checksum = new FNV32();
-        for (int b : INPUT_ZERO1) {
+        for (int b : INPUT32_ZERO1) {
             checksum.update(b);
         }
         Assert.assertEquals(0, checksum.getValue());
 
         checksum.reset();
-        for (int b : INPUT_ZERO2) {
+        for (int b : INPUT32_ZERO2) {
             checksum.update(b);
         }
         Assert.assertEquals(0, checksum.getValue());
@@ -25,11 +26,27 @@ public class FnvTest {
     @Test
     public void testFnv32UpdateBytesZeroHash() {
         FNV32 checksum = new FNV32();
-        checksum.update(INPUT_ZERO1, 0, INPUT_ZERO1.length);
+        checksum.update(INPUT32_ZERO1, 0, INPUT32_ZERO1.length);
         Assert.assertEquals(0, checksum.getValue());
 
         checksum.reset();
-        checksum.update(INPUT_ZERO2, 0, INPUT_ZERO1.length);
+        checksum.update(INPUT32_ZERO2, 0, INPUT32_ZERO1.length);
+        Assert.assertEquals(0, checksum.getValue());
+    }
+
+    @Test
+    public void testFnv64UpdateZeroHash() {
+        FNV64 checksum = new FNV64();
+        for (int b : INPUT64_ZERO) {
+            checksum.update(b);
+        }
+        Assert.assertEquals(0, checksum.getValue());
+    }
+
+    @Test
+    public void testFnv64UpdateBytesZeroHash() {
+        FNV64 checksum = new FNV64();
+        checksum.update(INPUT64_ZERO, 0, INPUT64_ZERO.length);
         Assert.assertEquals(0, checksum.getValue());
     }
 
