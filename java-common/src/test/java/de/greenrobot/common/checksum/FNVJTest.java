86d7a2133e4957855b871efda0b14a17e436169d
Markus <markus@greenrobot>
2014-10-26 01:33:40 +0200

    prepared testTrailingZero

--- java-common/src/test/java/de/greenrobot/common/checksum/FNVJTest.java
+++ java-common/src/test/java/de/greenrobot/common/checksum/FNVJTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 public class FNVJTest {
     private final static byte[] INPUT4 = {(byte) 0xcc, 0x24, 0x31, (byte) 0xc4};
-    private final static byte[] INPUT15 = {(byte) 0xe0, 0x4d, (byte) 0x9f, (byte) 0xcb, (byte) 0xd5, 0x6b,
-            (byte) 0xb9, 0x53, 0x42, (byte) 0x87, 0x08, 0x36, 0x77, 0x23, 0x01};
+    private final static byte[] INPUT16 = {(byte) 0xe0, 0x4d, (byte) 0x9f, (byte) 0xcb, (byte) 0xd5, 0x6b,
+            (byte) 0xb9, 0x53, 0x42, (byte) 0x87, 0x08, 0x36, 0x77, 0x23, 0x01, 0};
 
     @Test
     public void testBasics() {
@@ -40,15 +40,29 @@ public class FNVJTest {
     @Test
     public void testMixedUnaligned() {
         FNVJ32 checksum = new FNVJ32();
-        checksum.update(INPUT15, 0, INPUT15.length);
+        checksum.update(INPUT16, 0, INPUT16.length);
         long hash = checksum.getValue();
 
         checksum.reset();
-        checksum.update(INPUT15, 0, 2);
-        checksum.update(INPUT15[2]);
-        checksum.update(INPUT15, 3, 11);
-        checksum.update(INPUT15[14]);
+        checksum.update(INPUT16, 0, 2);
+        checksum.update(INPUT16[2]);
+        checksum.update(INPUT16, 3, 11);
+        checksum.update(INPUT16[14]);
+        checksum.update(INPUT16[15]);
         Assert.assertEquals(hash, checksum.getValue());
+    }
+
+    //@Test
+    public void testTrailingZero() {
+        FNVJ32 checksum = new FNVJ32();
+        long lastHash = checksum.getValue();
+        Assert.assertEquals(0, INPUT16[INPUT16.length-1]);
+        for (int b : INPUT16) {
+            checksum.update(b);
+            long hash = checksum.getValue();
+            Assert.assertNotEquals(lastHash, hash);
+            lastHash =hash;
+        }
     }
 
 }
