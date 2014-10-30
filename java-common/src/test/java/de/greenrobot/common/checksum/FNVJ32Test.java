e2e3c2560d2fc0156f20e0d11570c022f5d53025
Markus <markus@greenrobot>
2014-10-30 22:22:35 +0100

    added testSeed for FNVJ

--- java-common/src/test/java/de/greenrobot/common/checksum/FNVJ32Test.java
+++ java-common/src/test/java/de/greenrobot/common/checksum/FNVJ32Test.java
@@ -1,5 +1,6 @@
 package de.greenrobot.common.checksum;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FNVJ32Test extends AbstractChecksumTest {
@@ -10,6 +11,21 @@ public class FNVJ32Test extends AbstractChecksumTest {
     @Test
     public void testExpectedHash() {
         testExpectedHash(0x46B0F16BL, 0x252B0A1FL, 0x8AF88E69L);
+    }
+
+    @Test
+    public void testSeed() {
+        checksum.update(23);
+        long value = checksum.getValue();
+
+        FNVJ32 fnvj = new FNVJ32(1);
+        fnvj.update(23);
+        long valueSeeded = fnvj.getValue();
+        Assert.assertNotEquals(value, valueSeeded);
+
+        fnvj.reset();
+        fnvj.update(23);
+        Assert.assertEquals(valueSeeded, fnvj.getValue());
     }
 
 }
--- java-common/src/test/java/de/greenrobot/common/checksum/FNVJ64Test.java
+++ java-common/src/test/java/de/greenrobot/common/checksum/FNVJ64Test.java
@@ -1,5 +1,6 @@
 package de.greenrobot.common.checksum;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FNVJ64Test extends AbstractChecksumTest {
@@ -12,4 +13,18 @@ public class FNVJ64Test extends AbstractChecksumTest {
         testExpectedHash(-2788557096217532181L, 5189117314893555947L, 6178430581444874676L);
     }
 
+    @Test
+    public void testSeed() {
+        checksum.update(23);
+        long value = checksum.getValue();
+
+        FNVJ64 fnvj = new FNVJ64(1);
+        fnvj.update(23);
+        long valueSeeded = fnvj.getValue();
+        Assert.assertNotEquals(value, valueSeeded);
+
+        fnvj.reset();
+        fnvj.update(23);
+        Assert.assertEquals(valueSeeded, fnvj.getValue());
+    }
 }
