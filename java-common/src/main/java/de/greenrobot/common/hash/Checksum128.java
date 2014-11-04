dc282fdf9f5dc0c75af8d76ac6d212a2a0ce3dd3
Markus <markus@greenrobot>
2014-11-04 10:38:27 +0100

    checkstyle

--- java-common/src/main/java/de/greenrobot/common/hash/Checksum128.java
+++ java-common/src/main/java/de/greenrobot/common/hash/Checksum128.java
@@ -22,17 +22,17 @@ import java.util.zip.Checksum;
 /** Checksum interface to access 128 bit in various ways. */
 public interface Checksum128 extends Checksum {
     /** Returns the higher 64 bits of the 128 bit hash. */
-    public long getValueHigh();
+    long getValueHigh();
 
     /** Positive value. */
-    public BigInteger getValueBigInteger();
+    BigInteger getValueBigInteger();
 
     /** Padded with leading 0s to ensure length of 32. */
-    public String getValueHexString();
+    String getValueHexString();
 
     /** Big endian is the default in Java / network byte order. */
-    public byte[] getValueBytesBigEndian();
+    byte[] getValueBytesBigEndian();
 
     /** Big endian is used by most machines natively. */
-    public byte[] getValueBytesLittleEndian();
+    byte[] getValueBytesLittleEndian();
 }
