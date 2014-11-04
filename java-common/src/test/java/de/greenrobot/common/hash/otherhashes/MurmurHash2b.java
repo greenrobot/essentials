28d1538c85403b5ae3e8825e015ae0aa5f20646f
Markus <markus@greenrobot>
2014-11-04 10:23:13 +0100

    Apache License 2.0

--- java-common/src/test/java/de/greenrobot/common/hash/otherhashes/MurmurHash2b.java
+++ java-common/src/test/java/de/greenrobot/common/hash/otherhashes/MurmurHash2b.java
@@ -1,13 +1,11 @@
-package de.greenrobot.common.hash.otherhashes;
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/*
+ * Copyright (C) 2014 Markus Junginger, greenrobot (http://greenrobot.de)
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +14,7 @@ package de.greenrobot.common.hash.otherhashes;
  * limitations under the License.
  */
 
-
+package de.greenrobot.common.hash.otherhashes;
 /**
  * This is a very fast, non-cryptographic hash suitable for general hash-based
  * lookup. See http://murmurhash.googlepages.com/ for more details.
--- java-common/src/test/java/de/greenrobot/common/hash/otherhashes/MurmurHash3Yonik.java
+++ java-common/src/test/java/de/greenrobot/common/hash/otherhashes/MurmurHash3Yonik.java
@@ -1,21 +1,21 @@
+/*
+ * Copyright (C) 2014 Markus Junginger, greenrobot (http://greenrobot.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.greenrobot.common.hash.otherhashes;
 
-/**
- *  The MurmurHash3 algorithm was created by Austin Appleby.  This java port was authored by
- *  Yonik Seeley and is placed into the public domain.  The author hereby disclaims copyright
- *  to this source code.
- *  <p>
- *  This produces exactly the same hash values as the final C++
- *  version of MurmurHash3 and is thus suitable for producing the same hash values across
- *  platforms.
- *  <p>
- *  The 32 bit x86 version of this hash should be the fastest variant for relatively short keys like ids.
- *  <p>
- *  Note - The x86 and x64 versions do _not_ produce the same results, as the
- *  algorithms are optimized for their respective platforms.
- *  <p>
- *  See http://github.com/yonik/java_util for future updates to this file.
- */
 public class MurmurHash3Yonik {
 
   /** Returns the MurmurHash3_x86_32 hash. */
