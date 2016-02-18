5db6bf792b510796bd7004650b8920a3fd0ef6cf
Markus <markus@greenrobot>
2016-02-18 10:51:37 +0100

    added tests in CircularByteBufferTest

--- java-common/src/test/java/de/greenrobot/common/io/CircularByteBufferTest.java
+++ java-common/src/test/java/de/greenrobot/common/io/CircularByteBufferTest.java
@@ -52,6 +52,19 @@ public class CircularByteBufferTest {
     }
 
     @Test
+    public void testOffsetAndLen() {
+        int capacity = 16;
+        CircularByteBuffer buffer = new CircularByteBuffer(capacity);
+        byte[] bytes = createBytes(100);
+        assertEquals(10, buffer.put(bytes, 19, 10));
+        assertEquals(1, buffer.put(bytes, 49, 1));
+        assertEquals(5, buffer.put(bytes, 59, 50));
+        assertEquals(16, buffer.available());
+
+        getAndAssertEqualContent(buffer, new byte[]{20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 50, 60, 61, 62, 63, 64});
+    }
+
+    @Test
     public void testPutPartial() {
         int capacity = 16;
         CircularByteBuffer buffer = new CircularByteBuffer(capacity);
@@ -60,9 +73,24 @@ public class CircularByteBufferTest {
         assertEquals(6, buffer.put(bytes));
         assertEquals(0, buffer.put(bytes));
 
-        byte[] bytesGet = new byte[16];
-        assertEquals(16, buffer.get(bytesGet));
-        assertTrue(Arrays.equals(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6}, bytesGet));
+        getAndAssertEqualContent(buffer, new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6});
+    }
+
+    @Test
+    public void testGetPartial() {
+        int capacity = 16;
+        CircularByteBuffer buffer = new CircularByteBuffer(capacity);
+        byte[] bytes = createBytes(10);
+        buffer.put(bytes);
+
+        byte[] bytesGet = new byte[100];
+        assertEquals(10, buffer.get(bytesGet));
+        assertEquals(0, buffer.get(bytesGet));
+        assertEquals(0, buffer.get(bytesGet, 50, 10));
+
+        for (int i = 0; i < bytesGet.length; i++) {
+            assertEquals(i < bytes.length ? bytes[i] : 0, bytesGet[i]);
+        }
     }
 
     /** All possible start positions with all possible lengths */
@@ -71,18 +99,31 @@ public class CircularByteBufferTest {
         int capacity = 16;
         for (int startPosition = 0; startPosition <= capacity; startPosition++) {
             for (int length = 1; length <= capacity; length++) {
-                CircularByteBuffer buffer = new CircularByteBuffer(capacity);
-                byte[] prepBytes = new byte[startPosition];
-                assertEquals(startPosition, buffer.put(prepBytes));
-                assertEquals(startPosition, buffer.get(prepBytes));
+                for (int putLength1 = 0; putLength1 <= length; putLength1++) {
+                    for (int getLength1 = 0; getLength1 <= length; getLength1++) {
+                        CircularByteBuffer buffer = new CircularByteBuffer(capacity);
+                        byte[] prepBytes = new byte[startPosition];
+                        assertEquals(startPosition, buffer.put(prepBytes));
+                        assertEquals(startPosition, buffer.get(prepBytes));
 
-                byte[] bytes = createBytes(length);
-                assertEquals(length, buffer.put(bytes));
-                assertEquals(length, buffer.available());
-                byte[] bytesGet = new byte[length];
-                assertEquals(length, buffer.get(bytesGet));
-                assertEquals(0, buffer.available());
-                assertTrue(Arrays.equals(bytes, bytesGet));
+                        byte[] bytes = createBytes(length);
+                        assertEquals(putLength1, buffer.put(bytes, 0, putLength1));
+                        assertEquals(putLength1, buffer.available());
+                        int putLength2 = length - putLength1;
+                        assertEquals(putLength2, buffer.put(bytes, putLength1, putLength2));
+
+                        assertEquals(length, buffer.available());
+
+                        byte[] bytesGet = new byte[length];
+                        assertEquals(getLength1, buffer.get(bytesGet, 0, getLength1));
+                        int getLength2 = length - getLength1;
+                        assertEquals(getLength2, buffer.available());
+                        assertEquals(getLength2, buffer.get(bytesGet, getLength1, getLength2));
+
+                        assertTrue(Arrays.equals(bytes, bytesGet));
+                        assertEquals(0, buffer.available());
+                    }
+                }
             }
         }
     }
@@ -93,6 +134,12 @@ public class CircularByteBufferTest {
             bytes[i] = (byte) (i + 1);
         }
         return bytes;
+    }
+
+    private void getAndAssertEqualContent(CircularByteBuffer buffer, byte[] bytes) {
+        byte[] bytesGet = new byte[bytes.length];
+        assertEquals(bytes.length, buffer.get(bytesGet));
+        assertTrue(Arrays.equals(bytes, bytesGet));
     }
 
 }
