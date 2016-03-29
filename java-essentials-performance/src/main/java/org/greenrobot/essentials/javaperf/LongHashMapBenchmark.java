59a10ce6119ce86edbe515eae5b15ba0301ce45a
greenrobot-team <opensource+team@greenrobot.de>
2016-03-29 10:37:12 +0200

    fix #16 Pull Random outside of benchmark measurements

--- java-essentials-performance/src/main/java/org/greenrobot/essentials/javaperf/LongHashMapBenchmark.java
+++ java-essentials-performance/src/main/java/org/greenrobot/essentials/javaperf/LongHashMapBenchmark.java
@@ -9,7 +9,6 @@ public class LongHashMapBenchmark {
     static final int N = 100000;
     static final int WARM_UP_TIME_S = 5;
     static final int RUN_COUNT = 100;
-    static final long SEED = 657483918;
 
     // this is only for development purposes or to run tests separately. For automated benchmarking use gradle
     public static void main(String[] args) {
@@ -19,7 +18,19 @@ public class LongHashMapBenchmark {
     private LongHashMapBenchmark() {
     }
 
-    public static class LibImpl implements Runnable {
+    private static class BaseImpl {
+        final long[] values;
+
+        public BaseImpl() {
+            values = new long[N];
+            final Random random = new Random(657483918);
+            for (int i = 0; i < N; i++) {
+                values[i] = random.nextLong();
+            }
+        }
+    }
+
+    public static class LibImpl extends BaseImpl implements Runnable {
         private final int initialCapacity;
 
         public LibImpl(int initialCapacity) {
@@ -35,24 +46,21 @@ public class LongHashMapBenchmark {
             final LongHashMap<Object> map = new LongHashMap<>(initialCapacity);
             final Object object = new Object();
 
-            final Random random1 = new Random(SEED);
-            final Random random2 = new Random(SEED);
-            final Random random3 = new Random(SEED);
+            final long[] values = this.values;
 
             for (int i = 0; i < N; i++) {
-                final long key = random1.nextLong();
-                map.put(key, object);
+                map.put(values[i], object);
             }
             // check contains on every second key and remove it
             for (int i = 0; i < N; i++) {
-                final long key = random2.nextLong() + i % 2;
+                final long key = values[i] + i % 2;
                 if (map.get(key) != null) {
                     map.remove(key);
                 }
             }
             // remove the rest
             for (int i = 0; i < N; i++) {
-                final long key = random3.nextLong();
+                final long key = values[i];
                 if (map.get(key) != null) {
                     map.remove(key);
                 }
@@ -65,7 +73,7 @@ public class LongHashMapBenchmark {
         }
     }
 
-    public static class StdImpl implements Runnable {
+    public static class StdImpl extends BaseImpl implements Runnable {
         private final int initialCapacity;
 
         public StdImpl(int initialCapacity) {
@@ -81,24 +89,21 @@ public class LongHashMapBenchmark {
             final HashMap<Long, Object> map = new HashMap<>(initialCapacity);
             final Object object = new Object();
 
-            final Random random1 = new Random(SEED);
-            final Random random2 = new Random(SEED);
-            final Random random3 = new Random(SEED);
+            final long[] values = this.values;
 
             for (int i = 0; i < N; i++) {
-                final long key = random1.nextLong();
-                map.put(key, object);
+                map.put(values[i], object);
             }
             // check contains on every second key and remove it
             for (int i = 0; i < N; i++) {
-                final long key = random2.nextLong() + i % 2;
+                final long key = values[i] + i % 2;
                 if (map.get(key) != null) {
                     map.remove(key);
                 }
             }
             // remove the rest
             for (int i = 0; i < N; i++) {
-                final long key = random3.nextLong();
+                final long key = values[i];
                 if (map.get(key) != null) {
                     map.remove(key);
                 }
